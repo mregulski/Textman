7c4d9ab7efe49c3f11d4b3f61bae2f17ded817a4
Marcin Regulski <maarcin.regulski@gmail.com>
2017-03-08 01:44:33 +0100

    basic working version

--- app/src/main/java/ppt/reshi/textman/GameActivity.java
+++ app/src/main/java/ppt/reshi/textman/GameActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
@@ -25,9 +29,9 @@ public class GameActivity extends AppCompatActivity {
     private static final int GUESSES_INITIAL = 11;
 
     private LinearLayout mLettersContainer;
-    private EditText mLetterInput;
     private ArrayList<TextView> mLetters;
     private TextView mGuessesCounter;
+    private GridLayout mKeypad;
 
     private int mWordCount;
     private Random mRng;
@@ -43,12 +47,40 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         mLettersContainer = (LinearLayout) findViewById(R.id.ll_letters);
         mGuessesCounter = (TextView) findViewById(R.id.tv_guesses_label);
-        mLetterInput = (EditText) findViewById(R.id.et_pick);
-//        mLetterInput.addTextChangedListener(new LetterChecker());
+        mKeypad = (GridLayout) findViewById(R.id.gl_keypad);
+        findViewById(R.id.btn_new_game).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewGame();
+            }
+        });
         mWordCount = calculateWordCount();
         mRng = new Random();
+        createKeypad();
         startNewGame();
     }
+
+    private void createKeypad() {
+        char[] alphabet = getAlphabet("pl");
+        for (char c : alphabet) {
+            Button button = new Button(this);
+            GridLayout.LayoutParams params = (GridLayout.LayoutParams) button.getLayoutParams();
+            if (params == null) { params = new GridLayout.LayoutParams(); }
+            params.width = (int) getResources().getDimension(R.dimen.btn_letter_width);
+            button.setOnClickListener(new LetterListener());
+            button.setText(String.valueOf(c));
+            mKeypad.addView(button, params);
+        }
+    }
+
+    private char[] getAlphabet(String language) {
+        if (language.equals("pl")) {
+            return "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ".toCharArray();
+        } else {
+            throw new IllegalArgumentException("Unsupported language: " + language);
+        }
+    }
+
 
     private void startNewGame() {
         Resources res = getResources();
@@ -58,6 +90,7 @@ public class GameActivity extends AppCompatActivity {
         mLettersContainer.removeAllViews();
         mLetters = new ArrayList<>(mWord.length());
         mGuessesCounter.setText(res.getString(R.string.label_guesses, mGuessesLeft));
+        resetKeypad();
         for (int i = 0; i < mWord.length(); i++) {
             LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
             TextView letterView = (TextView) inflater.inflate(R.layout.game_letter_view, null);
@@ -66,24 +99,16 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Confirm button's clik handler.
-     * @param view
-     */
-    public void checkLetter(View view) {
-        if (mLetterInput.getText().length() > 0) {
-            checkLetter(mLetterInput.getText().charAt(0));
-            mLetterInput.setText("");
-        } else {
-            Log.d("checkLetter", "No letter to check.");
-        }
+    private void resetKeypad() {
+        mKeypad.removeAllViews();
+        createKeypad();
     }
 
     /**
      * Check if letter exists in the selected word and update UI accordingly.
      * @param letter - character to check
      */
-    private void checkLetter(char letter) {
+    private boolean checkLetter(char letter) {
         boolean isCorrect = false;
         for (int i = 0; i < mWord.length(); i++) {
             if (mWord.charAt(i) == Character.toUpperCase(letter)) {
@@ -94,21 +119,42 @@ public class GameActivity extends AppCompatActivity {
         }
         Log.d("checkLetter", "checking " + letter + ": " + (isCorrect ? "correct" : "incorrect"));
         Log.d("checkLetter", "correct: " + mLettersCorrect + "/" + mWord.length());
-        if (!isCorrect) {
-            guessIncorect();
-        }
+
+        return isCorrect;
+    }
+
+    private void guessCorrect(View v) {
+        v.setBackgroundColor(getColor(R.color.letter_bg_ok));
         if (mLettersCorrect == mWord.length()) {
-            Toast.makeText(this, "You won!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Super!", Toast.LENGTH_SHORT).show();
+//            startNewGame();
         }
     }
 
-    private void guessIncorect() {
+    private void guessIncorect(View v) {
         Resources res = getResources();
         mGuessesLeft --;
         mGuessesCounter.setText(res.getString(R.string.label_guesses, mGuessesLeft));
+        v.setBackgroundColor(getColor(R.color.letter_bg_wrong));
         if (mGuessesLeft <= 0) {
             // loss
-            startNewGame();
+            Toast.makeText(this, "Nie udało się :(", Toast.LENGTH_SHORT).show();
+            disableKeypad();
+            revealSolution();
+
+        }
+    }
+
+    private void revealSolution() {
+        for (int i = 0; i < mWord.length(); i++) {
+            mLetters.get(i).setText(String.valueOf(mWord.charAt(i)));
+        }
+    }
+
+    private void disableKeypad() {
+        int keyCount = mKeypad.getChildCount();
+        for (int i = 0; i < keyCount; i++) {
+            mKeypad.getChildAt(i).setEnabled(false);
         }
     }
 
@@ -152,32 +198,18 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-//    private class LetterChecker implements TextWatcher {
-//        private boolean mCleared = false;
-//        @Override
-//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//            /* do nothing */
-//        }
-//
-//        @Override
-//        public void onTextChanged(CharSequence s, int start, int before, int count) {
-//            /* do nothing */
-//        }
-//
-//        @Override
-//        public void afterTextChanged(Editable s) {
-//            if (s.length() <= 0) {
-//                return;
-//            }
-//            if (mCleared) {
-//                mCleared = false;
-//                return;
-//            }
-//            mCleared = true;
-//            checkLetter(s.charAt(0));
-//            mLetterInput.setText("");
-//
-//
-//        }
-//    }
+    private class LetterListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Log.d("Button", "Clicked: " + ((Button) v).getText());
+            v.setEnabled(false);
+
+            boolean isOk = checkLetter(((Button) v).getText().charAt(0));
+            if (isOk) {
+                guessCorrect(v);
+            } else {
+                guessIncorect(v);
+            }
+        }
+    }
 }
